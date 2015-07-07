8bbb0405602dbaf4fdfbc517d28173f3738f0f7d
Eric Arnebäck <erkastina@hotmail.com>
2015-07-07 16:33:43 +0200

    made function for drawing text circle.

--- src/texture_graph_node.cpp
+++ src/texture_graph_node.cpp
@@ -2,24 +2,23 @@
 
 
 TextureGraphNode::TextureGraphNode(wxWindow* parent, wxWindowID winid, const wxString& typeName, const wxPoint& pos):
-    wxPanel(parent, winid, pos, wxSize(NODE_WIDTH, NODE_HEIGHT), wxBORDER_NONE), m_typeName(typeName) {
+    wxPanel(parent, winid, pos, wxSize(NODE_WIDTH, NODE_HEIGHT), wxBORDER_NONE),
+    m_typeName(typeName),
+    m_circleFont(wxFontInfo(10).Bold())
+{
     Connect(wxEVT_PAINT, wxPaintEventHandler(TextureGraphNode::PaintEvent));
 
     image = new wxImage("perlin.png", wxBITMAP_TYPE_PNG);
     bitmap = new wxBitmap(*image);
+
+    m_backgroundBrush = wxBrush(wxColour("#d4d4d4"));
+    m_unselectedOutlinePen = wxPen(wxColour("#ffffff"));
 }
 
 void TextureGraphNode::Render(wxDC&  dc)
 {
-
-    wxColour gray;
-    gray.Set(wxT("#d4d4d4"));
-
-    wxColour white;
-    white.Set(wxT("#ffffff"));
-
-    dc.SetBrush(wxBrush(gray));
-    dc.SetPen(wxPen(white));
+    dc.SetBrush(m_backgroundBrush);
+    dc.SetPen(m_unselectedOutlinePen);
 
     dc.DrawRoundedRectangle(0, 0, NODE_WIDTH, NODE_HEIGHT, 10);
 
@@ -28,8 +27,7 @@ void TextureGraphNode::Render(wxDC&  dc)
    dc.DrawBitmap(*bitmap,5,30, false);
 
 
-
-   dc.DrawCircle(5, 100, 20);
+   DrawTextCircle(dc, NODE_WIDTH/2, NODE_HEIGHT-CIRCLE_RADIUS, "T");
 
 }
 
@@ -38,4 +36,14 @@ void TextureGraphNode::PaintEvent(wxPaintEvent & evt)
     // depending on your system you may need to look at double-buffered dcs
     wxPaintDC dc(this);
     Render(dc);
+}
+
+
+void TextureGraphNode::DrawTextCircle(wxDC& dc, const wxCoord x, const wxCoord y, const wxString& str) {
+   dc.DrawCircle(x, y, CIRCLE_RADIUS);
+   dc.SetFont(m_circleFont);
+
+   wxSize sz = dc.GetTextExtent(str);
+
+   dc.DrawText( str, x - sz.GetWidth() + CIRCLE_RADIUS/2, y - sz.GetHeight() + 6);
 }
--- src/texture_graph_node.hpp
+++ src/texture_graph_node.hpp
@@ -7,12 +7,29 @@ class TextureGraphNode : public wxPanel{
 
 private:
 
+    /*
+      INSTANCE VARIABLES
+     */
+
     wxPanel *m_parent;
 
     wxString m_typeName;
 
     wxImage* image;
     wxBitmap* bitmap;
+
+
+    wxFont m_circleFont;
+
+    wxBrush m_backgroundBrush;
+    wxPen m_unselectedOutlinePen;
+
+    /*
+      INSTANCE METHODS:
+     */
+
+    void DrawTextCircle(wxDC& dc, const wxCoord x, const wxCoord y, const wxString& str);
+
 
 public:
     TextureGraphNode(wxWindow* parent, wxWindowID winid, const wxString& typeName, const wxPoint& pos);
@@ -25,4 +42,6 @@ public:
     */
     constexpr static int NODE_WIDTH = 80;
     constexpr static int NODE_HEIGHT = 110;
+    constexpr static int CIRCLE_RADIUS = 7;
+
 };

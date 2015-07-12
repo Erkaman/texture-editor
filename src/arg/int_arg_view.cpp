588f6bc150e53c3111bf523a56f83b88c951cfae
Eric Arnebäck <arnebackeric@gmail.com>
2015-07-12 13:40:50 +0200

    Added a text field to IntArgView.

--- src/arg/int_arg_view.cpp
+++ src/arg/int_arg_view.cpp
@@ -4,6 +4,7 @@
 
 #include <wx/sizer.h>
 #include <wx/button.h>
+#include <wx/textctrl.h>
 
 IntArgView::IntArgView(wxWindow *parent, IntArg* model): ArgView(parent), m_model(model) {
 
@@ -12,9 +13,11 @@ IntArgView::IntArgView(wxWindow *parent, IntArg* model): ArgView(parent), m_mode
     wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
     m_minusButton = new wxButton(this, wxID_ANY, wxT("-"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
+    m_intTextCtrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(-1,-1), wxTE_PROCESS_ENTER);
     m_plusButton = new wxButton(this, wxID_ANY, wxT("+"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
 
     hbox->Add(m_minusButton, 1, wxALL, 5);
+    hbox->Add(m_intTextCtrl, 2, wxALL, 5);
     hbox->Add(m_plusButton, 1, wxALL, 5);
     hbox->SetSizeHints(this);
 
